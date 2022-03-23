8f26dd0bbed1fd2a260fde6a0fcb270ff863f597
Valera Sokolov <komproekt42@gmail.com>
2022-03-23 09:25:28 +0300

    Version2

--- src/js/GameSavingLoader.js
+++ src/js/GameSavingLoader.js
@@ -6,22 +6,16 @@ export default class GameSavingLoader {
     return new Promise((resolve, reject) => {
       let data = read();
       let value;
-      setTimeout(() => {
-        data.then(
-          function(result) { 
-            value = json(result)
-          },
-        );
-      }, 2000);
-      setTimeout(() => {
-        value.then(
-          function(result) { 
-            resolve(JSON.parse(result))
-          }, 
-        );
-      }, 4000);
+      data.then(
+        function (result) {
+          value = json(result)
+          return value
+        },
+      ).then(
+        function (result) {
+          resolve(JSON.parse(result))
+        },
+      );
     });
   }
 }
-
-
--- src/js/__tests__/BasicTest.js
+++ src/js/__tests__/BasicTest.js
@@ -2,17 +2,17 @@ import GameSavingLoader from '../GameSavingLoader.js';
 
 const gamasav = new GameSavingLoader()
 
-test('should work with promise', async () => {
-     const data = await gamasav.load()
-     const obj = {
-        "id":9,
-        "created":1546300800,
+test('Проверка промис без async / await ', () => {
+    const data = gamasav.load()
+    const obj = {
+        "id": 9,
+        "created": 1546300800,
         "userInfo": {
-            "id":1,
-            "name":"Hitman",
-            "level":10,
-            "points":2000
+            "id": 1,
+            "name": "Hitman",
+            "level": 10,
+            "points": 2000
         }
-     }
-     expect(data).toEqual(obj);
-     });
+    }
+    return expect(data).resolves.toEqual(obj);
+});
